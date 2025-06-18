6e4c6b4ec4523760d767be6ae60c08af93545883
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-06-18 16:48:21 +0200

    Done with the mobile nav section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import MobileNav from "./MobileNav"
 
 
 export const Header = () => {
@@ -8,6 +9,9 @@ export const Header = () => {
             <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
                 MernEats.com
             </Link>
+            <div className="md:hidden">
+               <MobileNav />
+            </div>
         </div>
     </div>
   )
