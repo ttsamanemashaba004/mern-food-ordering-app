69fa6f064fe164117b73b11f63c0ce79efea427c
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-06-18 16:53:38 +0200

    Desktop Nav Login button added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import MobileNav from "./MobileNav"
+import MainNav from "./MainNav"
 
 
 export const Header = () => {
@@ -11,6 +12,9 @@ export const Header = () => {
             </Link>
             <div className="md:hidden">
                <MobileNav />
+            </div>
+            <div className="hidden md:block">
+              <MainNav/>
             </div>
         </div>
     </div>
