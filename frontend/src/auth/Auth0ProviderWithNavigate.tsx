7f342d6cc71186ffefe971057ecd30c1152ce335
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-06-18 17:30:38 +0200

    Busy with auth0 setup timestamp: 1h34m09

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Auth0ProviderWithNavigate = props => {
+  return (
+    <div>Auth0ProviderWithNavigate</div>
+  )
+}
+
+Auth0ProviderWithNavigate.propTypes = {}
+
+export default Auth0ProviderWithNavigate
